8be036c4949c36c6b27258bc9bb7270e9e924bf0
Lorwik <lorwik@gmail.com>
2026-01-29 00:17:03 +0100

    feat: enable undo/redo for map properties editing

---
+++ src/main/java/org/argentumforge/engine/utils/editor/commands/MapPropertiesChangeCommand.java
@@ -0,0 +1,49 @@
+package org.argentumforge.engine.utils.editor.commands;
+
+import org.argentumforge.engine.utils.GameData;
+import org.argentumforge.engine.utils.inits.MapProperties;
+
+/**
+ * Comando para gestionar cambios en las propiedades generales del mapa.
+ */
+public class MapPropertiesChangeCommand implements Command {
+
+    private final MapProperties oldProperties;
+    private final MapProperties newProperties;
+
+    /**
+     * Crea un nuevo comando de cambio de propiedades.
+     * 
+     * @param oldProps Copia de las propiedades antes del cambio.
+     * @param newProps Copia de las propiedades después del cambio.
+     */
+    public MapPropertiesChangeCommand(MapProperties oldProps, MapProperties newProps) {
+        this.oldProperties = oldProps;
+        this.newProperties = newProps;
+    }
+
+    @Override
+    public void execute() {
+        applyProperties(newProperties);
+    }
+
+    @Override
+    public void undo() {
+        applyProperties(oldProperties);
+    }
+
+    private void applyProperties(MapProperties source) {
+        if (GameData.mapProperties == null)
+            return;
+
+        GameData.mapProperties.setName(source.getName());
+        GameData.mapProperties.setMusicIndex(source.getMusicIndex());
+        GameData.mapProperties.setMagiaSinEfecto(source.getMagiaSinEfecto());
+        GameData.mapProperties.setNoEncriptarMP(source.getNoEncriptarMP());
+        GameData.mapProperties.setPlayerKiller(source.getPlayerKiller());
+        GameData.mapProperties.setRestringir(source.getRestringir());
+        GameData.mapProperties.setBackup(source.getBackup());
+        GameData.mapProperties.setZona(source.getZona());
+        GameData.mapProperties.setTerreno(source.getTerreno());
+    }
+}
